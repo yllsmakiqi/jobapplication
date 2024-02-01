a81f5a1871d0efff36975a427fdf9a356d7911a0
yllsmakiqi <120490359+yllsmakiqi@users.noreply.github.com>
2024-02-02 00:03:50 +0100

    header footer of the webpage

---
+++ components/footer.php
@@ -0,0 +1,18 @@
+<div class="footer-main">
+      
+      <div class="footer-blloku">
+          <p>Faqja jone ka per qellim te ju ofroj oferta nga me te ndryshme pune dhe presim qe ju te ndaheni te kenaqur me neve!</p>
+      </div>
+      
+      <div class="footeridy">
+      <ul>
+            <li><a href="https://www.instagram.com/?hl=en"><img src="images/instalogo.png"></a></li>
+            <h1></h1>
+            <li><a href="https://www.facebook.com/"><img src="images/facebooklogo.png"></a></li>
+            <h1></h1>
+            <li><a href="https://twitter.com/?lang=en"><img src="images/twitterlogo.png"></a></li>
+          </ul>
+      </div>
+    </div>
+</body>
+</html>
