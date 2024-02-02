ab65a22f1d0dbbb410a993afda2d939975390181
gentianaymeri1 <gentianaymeri1@gmail.com>
2024-02-02 01:48:26 +0100

    aboutus

---
+++ aboutus.php
@@ -0,0 +1,69 @@
+<?php include 'components/header.php' ?>   
+<div class="main-about">
+    <div class="div-majt-about">
+        
+        <div class="divMajtKontenti-home">
+            
+            <div class="divfotoja">
+                <!-- Replace the existing image with "thh.jpg" from the "images" folder -->
+                <img id="divMajtFoto" src="images/thh.jpg" alt="Your Alt Text">
+            </div>
+            
+            <div class="divtextt">
+                <p> </p>
+            </div>
+
+            <div class="divMbuttonii">
+                <a href="Job.php">Shiko punet</a>
+            </div>
+
+        </div>
+    </div>
+    
+    <div class="div-djatht-home">
+        <div class="djathkontenti">
+            <!-- Replace the existing image with "thh.jpg" from the "images" folder -->
+            <img id="fotohome" src="images/tth.jpg" alt="Your Alt Text">
+        </div>
+    </div> 
+</div>
+
+<div class="mainposht-home">
+    <div class="divnalt-home">
+        <h2>Cfare bejme ne</h2>
+        <p>Ne jemi nje faqe online ku qellimi yne eshte te ju ndihmoj per te ofruar qe te gjeni punen tuaj te preferuar </p>
+    </div>
+    
+    <div class="divposht-home">
+        
+        <div class="ilustrimespec">
+            <h3>1/ Search for job</h3>
+            <p>Ne ju ofrojme pozita te ndryshme te punes qe jane te ofruara nga kompani te ndryshme</p>
+        </div>
+        <h1></h1>
+        
+        <div class="favoritet">
+            <h3>2/ Apply a job</h3>
+            <p>Ne deshirojm qe te gjeni nje pozite pune qe ju deshironi ne menyre sa me te lehte</p>
+        </div>
+        <h1></h1>
+        
+        <div class="transporti">
+            <h3>3/ Start working</h3>
+            <p>Shpresoj qe te ndaheni te kenaqur nga ofertat tona </p>
+        </div>
+    </div>
+    
+    <div class="mainimaposht-home">
+        <h3>Ekipi jone</h3>
+        <ul>
+            <li><h4>Ylli</h4></li>
+            <h1></h1>
+            <li><h4>Gentiana</h4></li>
+            <h1></h1>
+            <li><h4>Velsa</h4></li>
+        </ul>
+    </div>
+</div>
+
+<?php include 'components/footer.php' ?>
